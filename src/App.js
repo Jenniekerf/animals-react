1f4d5b0652fd0122407a300c0641050587fd809e
Jenniekerf <jenniekerf@gmail.com>
2019-11-11 16:24:29 -0500

    added home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ListOfAnimals from './containers/ListOfAnimals';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/animals" component={ListOfAnimals} />
+      </Router>
+
     </div>
   );
 }
