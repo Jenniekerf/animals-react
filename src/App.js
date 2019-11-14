fe1f212d88b6a701997c131ae7a9eb383e1cfdae
Jenniekerf <jenniekerf@gmail.com>
2019-11-13 19:36:29 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import NewAnimal from './components/NewAnimal';
-import AnimalShow from './components/AnimalShow'
+import AnimalShow from './components/AnimalShow';
+import Navbar from './components/Navbar';
 import ListOfAnimals from './containers/ListOfAnimals';
 import { connect } from 'react-redux';
-import { getAnimals } from './redux/actions/animals'
+import { getAnimals } from './redux/actions/animals';
 
 class App extends React.Component {
   componentDidMount() {
@@ -17,7 +18,9 @@ class App extends React.Component {
   render() {
     return (
     <div className="App">
+
       <Router>
+        <Navbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/animals" component={ListOfAnimals} />
         <Route path="/animals/new" component={NewAnimal} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav>
+    <ul>
+      <Link to="/">Home</Link>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
