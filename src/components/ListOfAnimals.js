66ca588f6eebf9c110612879d0554975036e9d01
Jenniekerf <jenniekerf@gmail.com>
2019-11-28 10:03:49 -0500

    adjusting lists

--- src/components/ListOfAnimals.js
+++ src/components/ListOfAnimals.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
+class ListOfAnimals extends Component {
 
-const ListOfAnimals = (props) => {
+  render() {
+    if (this.props.animals.length === 0) {
+      return <p className="lostandfound-wrapper" id="alert">There are currently no animals added</p>;
+    }
     return (
-
       <div className="lostandfound-wrapper">
         <h1 className="container__title">List Of Animals</h1>
-        {props.animals.map(animal => (
+        {this.props.animals.map(animal => (
             <p key={animal.id}>
               <Link to={`/animals/${animal.id}`}>{animal.name}</Link></p>
-              ))}
+          ))}
+          
       </div>
     );
   }
+}
 
 const mapStateToProps = (state) => {
 return {
